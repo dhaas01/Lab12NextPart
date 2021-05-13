362b2b674db9151cd9dad1f0cba6f452c248ea7b
dhaas01 <77082867+dhaas01@users.noreply.github.com>
2021-05-12 22:34:27 -0400

    Add files via upload

---
+++ Lab12_2/Lab12_2/ChildrensMovie.java
@@ -0,0 +1,17 @@
+public class ChildrensMovie extends Movie {
+   // Implement constructor
+   public ChildrensMovie(String title) {
+      // write code here - Lab 12
+   }
+   
+   // Implement amountFor method
+   public double amountFor(int daysRented) {
+      // write code here - Lab 12
+      return 0;
+   }
+   
+   public int frequentRenterPointsFor(int daysRented) {
+      return 1;
+   }
+
+}
---
+++ Lab12_2/Lab12_2/NewReleaseMovie.java
@@ -0,0 +1,18 @@
+public class NewReleaseMovie extends Movie {
+   // Implement constructor
+   public NewReleaseMovie(String title) {
+		// write code here - Lab 12
+   }
+   
+   // Implement amountFor method
+   public double amountFor(int daysRented) {
+   // write code here - Lab 12
+      return 0;
+   }
+   
+   public int frequentRenterPointsFor(int daysRented) {
+      return 1;
+   }
+
+
+}
